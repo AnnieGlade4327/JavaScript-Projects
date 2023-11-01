d0e9b545e4bd3e905ecc286cea68543fae040d46
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 15:53:51 -0600

    project3 assign submission, semi-functional

--- Basic Javascript Projects/Project3_math_operators/mainProject3.js
+++ Basic Javascript Projects/Project3_math_operators/mainProject3.js
@@ -1,5 +1,11 @@
+
+var num1 = "2";
+var num2 = "2";
+var sum = 4;
+document.write("The sum of 2 and 2 is " + sum);
+
 var Math = "addition";
 function addition_Function() {
   var addition = 2 + 2;
-  document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+  document.getElementById("math").innerHTML = "2 + 2 = " + addition;
 }
