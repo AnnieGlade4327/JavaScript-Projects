1604bf3146d840104810fd87ec50e5efb11d6972
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 10:27:03 -0600

    saved alert in js file

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+alert("Hello World!"); //Also, did you notice we created a string? As we mentioned earlier, 
+//strings are one of the seven value types. In JavaScript, we can create strings with double quotes (“) or single quotes (‘), 
+//such as: “Mary had a little lamb,” or ‘Its fleece was white as snow.’ 
+//In our above code, “Hello, World!” is a string. It doesn’t matter whether you use double or single quotes; 
+//they both perform the same functions.
