87c6471c7b0f02a86647e6bf3034df7eb8f65762
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 10:33:06 -0600

    variable hello

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,12 @@
-alert("Hello World!"); //Also, did you notice we created a string? As we mentioned earlier, 
+alert("Hello World! 1 ");
+
+window.alert("Hello World! 2"); //Also, did you notice we created a string? As we mentioned earlier, 
 //strings are one of the seven value types. In JavaScript, we can create strings with double quotes (“) or single quotes (‘), 
 //such as: “Mary had a little lamb,” or ‘Its fleece was white as snow.’ 
 //In our above code, “Hello, World!” is a string. It doesn’t matter whether you use double or single quotes; 
 //they both perform the same functions.
+
+document.write('Hello World! 3');
+
+var X = " Hello, World!";
+document.write(X);
