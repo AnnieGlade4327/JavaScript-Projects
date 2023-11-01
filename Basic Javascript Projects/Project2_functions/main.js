3821ff7d62d30e7865578dcd2202a6792e4067f2
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 12:40:58 -0600

    button onClick

---
+++ Basic Javascript Projects/Project2_functions/main.js
@@ -0,0 +1,11 @@
+
+window.alert("Hello World!");
+function my_first_function() {
+    var str = "This Will be the Text of the Button, or Whatever is to display/link";
+    document.getElementById("button_text").innerHTML =str;
+}
+function my_second_function() {
+    var str = "and to see if it will go back when you click again";
+    document.getElementById("button_text2").innerHTML =str;
+}
+//probably has to be a way to make it so you can click through and display diff texts each time? 
