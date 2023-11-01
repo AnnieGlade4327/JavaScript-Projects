aa00179e4379f634473146d432efe8ce4cd0f20e
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 16:59:03 -0600

    saving for object assign

--- Basic Javascript Projects/Project3_math_operators/mainProject3.js
+++ Basic Javascript Projects/Project3_math_operators/mainProject3.js
@@ -1,2 +1,39 @@
 
-document.getElementById("mathObjects").innerHTML = Math.round(4.6);
+
+function display(mathObjects) {
+  document.getElementById("mathObjects").innerHTML = Math.round(4.6);
+}
+
+function display(val) {
+  document.getElementById("result").value += val;
+
+  return val;
+}
+
+function solve() {
+  let x = document.getElementById("result").value;
+
+  let y = eval(x);
+
+  document.getElementById("result").value = y;
+
+  return y;
+}
+
+function negation_Operator() {
+  var x = 10;
+  document.getElementById("Math").innerHTML = -x;
+  document.write(Math);
+}
+
+window.alert(Math.random());
+window.alert(Math.random() * 100);
+
+var Q = 5;
+Q++;
+document.write(Q);
+var P = 5.25;
+P--;
+document.write(P);
+
+
