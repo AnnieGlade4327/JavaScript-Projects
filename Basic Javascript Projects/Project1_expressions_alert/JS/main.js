a31034d9e1ab7e975d491fee1cc29a6edba4d79f
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 16:21:58 -0600

    i don't know why i can't get the code to work from the course, but if i do it this way I can get it to work

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,55 +1,54 @@
 alert("Hello World! 1 ");
 
-window.alert("Hello World! 2"); //Also, did you notice we created a string? As we mentioned earlier, 
-//strings are one of the seven value types. In JavaScript, we can create strings with double quotes (“) or single quotes (‘), 
-//such as: “Mary had a little lamb,” or ‘Its fleece was white as snow.’ 
-//In our above code, “Hello, World!” is a string. It doesn’t matter whether you use double or single quotes; 
+window.alert("Hello World! 2"); //Also, did you notice we created a string? As we mentioned earlier,
+//strings are one of the seven value types. In JavaScript, we can create strings with double quotes (“) or single quotes (‘),
+//such as: “Mary had a little lamb,” or ‘Its fleece was white as snow.’
+//In our above code, “Hello, World!” is a string. It doesn’t matter whether you use double or single quotes;
 //they both perform the same functions.
 
-document.write('Hello World! 3');
+document.write("Hello World! 3");
 
 var X = " Hello, World!";
 document.write(X);
 
-var frog = " This is a list of frogs"
+var frog = " This is a list of frogs";
 document.write(frog);
 
 var stringTest = "This is a string";
 window.alert(stringTest);
 
-document.write(" <br> Nadia told Lazlo, <br> \"I cannot even utter his Name--\" <br> \"I don't give a f***, his name was Mike.\" Lazlo responded.");
+document.write(
+  ' <br> Nadia told Lazlo, <br> "I cannot even utter his Name--" <br> "I don\'t give a f***, his name was Mike." Lazlo responded.'
+);
 
-document.write("<br> \"This is how we talk in Tuscan Arizoña\" -Lazlo"
-+ " <br> \"We drank the blood of some people, but the people were on drugs," 
-+ " and now I\'m a wizard.\" -Nandor");
+document.write(
+  '<br> "This is how we talk in Tuscan Arizoña" -Lazlo' +
+    ' <br> "We drank the blood of some people, but the people were on drugs,' +
+    " and now I'm a wizard.\" -Nandor"
+);
 
 var B = "<br>Concatenated" + " String";
 document.write(B); //concatenated string value added to variable and displayed.
 
-var vampiricCouncil = "<br>Vampiric Council:<br>", coChairs = "Nadia and Nandor<br>", coCoChair = "Lazlo<br>",
-assistant = "Guillermo<br>";
+var vampiricCouncil = "<br>Vampiric Council:<br>",
+  coChairs = "Nadia and Nandor<br>",
+  coCoChair = "Lazlo<br>",
+  assistant = "Guillermo<br>";
 document.write(vampiricCouncil);
 document.write(coChairs);
 document.write(coCoChair);
 document.write(assistant);
 
 document.write(3 + 7);
-document.write(3*3*43);
+document.write(3 * 3 * 43);
 //Here is an actual function (this is JavaScript code – it would be written within the external .js file):
 
 function my_first_function() {
-    var str = "this is the button txt";
-    document.getElementById("button_text").innerHTML =str;
+  var str = "this is the button txt";
+  document.getElementById("button_text").innerHTML = str;
 }
 
 var sent1 = "<br>This is the beginning of the string";
 var sent2 = "<br>and this is the end of the string";
-document.write(sent1
-    + sent2);
+document.write(sent1 + sent2);
 
-
-var Math = "Addition";
-    function addition_Function() {
-        var Addition = 2 + 2;
-        document.getElementById("Math").innerHTML = "2 + 2 = " + Addition;
-    }
--- Basic Javascript Projects/Project3_math_operators/mainProject3.js
+++ Basic Javascript Projects/Project3_math_operators/mainProject3.js
@@ -1,11 +1,25 @@
-
-var num1 = "2";
-var num2 = "2";
-var sum = 4;
-document.write("The sum of 2 and 2 is " + sum);
-
 var Math = "addition";
-function addition_Function() {
+function addition() {
   var addition = 2 + 2;
-  document.getElementById("math").innerHTML = "2 + 2 = " + addition;
+  document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
+
+function subtraction() {
+  var subtraction = 5 - 2;
+  document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
+}
+function display(val) {
+  document.getElementById("result").value += val;
+
+  return val;
+}
+
+function solve() {
+  let x = document.getElementById("result").value;
+
+  let y = eval(x);
+
+  document.getElementById("result").value = y;
+
+  return y;
 }
