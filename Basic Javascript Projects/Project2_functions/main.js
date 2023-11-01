5f7ac7e9fc50e6d46db214083bea58152d526ccf
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 12:48:07 -0600

    saving function attempt

--- Basic Javascript Projects/Project2_functions/main.js
+++ Basic Javascript Projects/Project2_functions/main.js
@@ -6,6 +6,15 @@ function my_first_function() {
 }
 function my_second_function() {
     var str = "and to see if it will go back when you click again";
-    document.getElementById("button_text2").innerHTML =str;
+    document.getElementById("button_text2").innerHTML = str;
 }
 //probably has to be a way to make it so you can click through and display diff texts each time? 
+
+//+- will concatenate a string. <p id="Concatenate"></p> //assigning an id in HTML
+<p id= "Concatenate" onClick = "testFunction()">Click Here!</p>
+
+function testFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
