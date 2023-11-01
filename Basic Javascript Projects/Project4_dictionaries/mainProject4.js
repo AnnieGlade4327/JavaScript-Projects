cdd412be183a112b76660c21468fc02284cc1698
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 17:43:39 -0600

    saving proj4 assign

--- Basic Javascript Projects/Project4_dictionaries/mainProject4.js
+++ Basic Javascript Projects/Project4_dictionaries/mainProject4.js
@@ -8,3 +8,16 @@ function this_Dictionary() {
     };
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
 }
+
+function this_Dictionary() {
+    var Animal = {
+        Class: "Mammalia",
+        Genus: "Antilocapra",
+        Species: "Antilocapra Americana-Pronghorn",
+        Speed: "~65 Miles Per Hour",
+        Sound: "tick, tchick, tcherrick",
+    };
+    delete Animal.Class;
+    document.getElementById("Dictionary").innerHTML = Animal.Class;
+}
+
