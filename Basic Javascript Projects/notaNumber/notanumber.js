14ea8e78680e245e64a01b6f1529e81c5fae0710
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 19:39:23 -0600

    true false test

--- Basic Javascript Projects/notaNumber/notanumber.js
+++ Basic Javascript Projects/notaNumber/notanumber.js
@@ -1,22 +1,24 @@
-console.log(2 +2);
+console.log(2 + 2);
 function my_Function() {
   document.getElementById("Test").innerHTML = 0 / 0;
 }
 
 function my_Function() {
-  document.getElementById("Test2").innerHTML = isNaN('this is a string');
+  document.getElementById("Test2").innerHTML = isNaN("this is a string");
 }
 
 function my_Function() {
-    document.getElementById("Test3").innerHTML = isNaN('007');
-  }
-  
-  document.write(2E310);
-  document.write(-3E310);
+  document.getElementById("Test3").innerHTML = isNaN("007");
+}
 
-  document.write(10>2);
+document.write(2e310);
+document.write(-3e310);
 
-  document.write(10<2);
+document.write(10 > 2);
 
+document.write(10 < 2);
 
-console.log('false');
+console.log("false");
+
+document.write(10==10);
+document.write(3==11);
