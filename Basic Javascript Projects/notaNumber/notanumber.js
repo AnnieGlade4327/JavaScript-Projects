e9207bc5222c91bde386f43ece2641b5da1fe552
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-01 19:22:04 -0600

    saving

---
+++ Basic Javascript Projects/notaNumber/notanumber.js
@@ -0,0 +1,14 @@
+function my_Function() {
+  document.getElementById("Test").innerHTML = 0 / 0;
+}
+
+function my_Function() {
+  document.getElementById("Test2").innerHTML = isNaN('this is a string');
+}
+
+function my_Function() {
+    document.getElementById("Test3").innerHTML = isNaN('007');
+  }
+  
+  document.write(2E310);
+  document.write(-3E310);
