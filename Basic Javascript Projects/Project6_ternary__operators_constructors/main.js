4879db9251cf9a841baf7d277c1f4e62dd373775
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 11:58:46 -0600

    added comments

--- Basic Javascript Projects/Project6_ternary__operators_constructors/main.js
+++ Basic Javascript Projects/Project6_ternary__operators_constructors/main.js
@@ -4,15 +4,15 @@ function Age_Function() {
     Can_vote = (Age < 18) ? "You are not old enough":"You are old enough";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
-
+//assigning the nested function and calling the ID
 function Nest_Function() 
     {
-    document.getElementById("Nesting").innerHTML = Count();
+    document.getElementById("Nesting").innerHTML = Count(); //now when Nesting is retrieved, it will begin the Count function
     function Count() {
-    var Starting_point = 9;
-    function Plus_one()
+    var Starting_point = 9; //starting at 9
+    function Plus_one() //adding 1
     {Starting_point += 1;}
     Plus_one();
-    return Starting_point; 
+    return Starting_point; //returns the new amnt which should display as 10, 9 (starting point) + 1.
         }
     }
