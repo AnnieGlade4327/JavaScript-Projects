478171877462f0245c98256f4f4058d1dedc6ce1
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 08:43:39 -0600

    not operator true and false

--- Basic Javascript Projects/notaNumber/notanumber.js
+++ Basic Javascript Projects/notaNumber/notanumber.js
@@ -43,3 +43,10 @@ document.write(5 > 10 && 10> 4);
 document.write(5 > 10 || 10 > 4);
 
 document.write(5 >  10 || 10 > 20);
+
+function not_Function() {
+  document.getElementById("Not").innerHTML = ! (20>10);
+}
+function not_Function() {
+  document.getElementById("Not").innerHTML = ! (5>10);
+}
