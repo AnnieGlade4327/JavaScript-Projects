7f818c8878cb03f6655369699914a92014effd93
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 11:52:45 -0600

    added/assigned a nesting function

--- Basic Javascript Projects/Project6_ternary__operators_constructors/main.js
+++ Basic Javascript Projects/Project6_ternary__operators_constructors/main.js
@@ -4,3 +4,15 @@ function Age_Function() {
     Can_vote = (Age < 18) ? "You are not old enough":"You are old enough";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
+
+function Nest_Function() 
+    {
+    document.getElementById("Nesting").innerHTML = Count();
+    function Count() {
+    var Starting_point = 9;
+    function Plus_one()
+    {Starting_point += 1;}
+    Plus_one();
+    return Starting_point; 
+        }
+    }
