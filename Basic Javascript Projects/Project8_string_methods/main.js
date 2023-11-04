8bd7435ac0f2b0a346a38ac57e23af3ce397cfda
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 15:12:49 -0600

    adding properties to objects

---
+++ Basic Javascript Projects/Project8_string_methods/main.js
@@ -0,0 +1,9 @@
+function full_sentence() {
+    var part_1 = "I have "
+    var part_2
+    var part_3
+    var part_4
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+    }
+    
