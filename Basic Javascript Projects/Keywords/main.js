cc4c1e30abea3c94c93f2e833136c8687091b99d
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 10:35:35 -0600

    saving updates

--- Basic Javascript Projects/Keywords/main.js
+++ Basic Javascript Projects/Keywords/main.js
@@ -1,6 +1,11 @@
+//assigning the function
 function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
     this.Vehicle_Color = Color;
 }
+//assigning objects to keywords
+var Jack
+var Emily
+var Erik
