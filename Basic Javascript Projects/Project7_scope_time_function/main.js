c51f9772f60a38d5293776c551f49486095e6eed
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 13:00:59 -0600

    proj7 assign submission

--- Basic Javascript Projects/Project7_scope_time_function/main.js
+++ Basic Javascript Projects/Project7_scope_time_function/main.js
@@ -1,33 +1,39 @@
 //Global Variable
 var X = 10;
 function Add_Numbers_1() {
-document.write(20 + X + "<br>");
+  document.write(20 + X + "<br>");
 }
 function Add_Numbers_2() {
-document.write(X = 100);
+  document.write((X = 100));
 }
 Add_Numbers_1();
 Add_Numbers_2();
 
 //Local variable
 function Add_numbers_1() {
-    var x = 10;
-    document.write(20 + X + "<br>");
-    }
-    function Add_numbers_2() {
-    document.write(X + 100);
-    }
-    Add_numbers_1();
-    Add_numbers_2();
-    
+  var x = 10;
+  document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+  document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
 //using console.log to check for errors
 function Add_numbers_1() {
-    var x = 10;
-    console.log(15 + X);
+  var x = 10;
+//   console.log(15 + X);
+}
+function Add_numbers_2() {
+//   console.log(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How is your day today?";
     }
-    function Add_numbers_2() {
-    console.log(X + 100);
-    }
-    Add_numbers_1();
-    Add_numbers_2();
-    
+    console.log("Greetings");
+}
