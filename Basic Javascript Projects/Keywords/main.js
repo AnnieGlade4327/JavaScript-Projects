dbe122f65dd737f791ea2a8ba0c7fd7c7010b0d8
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 11:18:29 -0600

    made a new function and assigned a constructor/variable

--- Basic Javascript Projects/Keywords/main.js
+++ Basic Javascript Projects/Keywords/main.js
@@ -9,7 +9,21 @@ function Vehicle(Make, Model, Year, Color) {
 var Jack = new Vehicle("Dodge", "Viper,", 2020, "Red ");
 var Emily = new Vehicle("Jeep", "Trail Hawk,", 2019, "White and Black ");
 var Erik = new Vehicle("Ford", "Pinto,", 1971, "Mustard");
-function myFunction () {
+function myFunction() {
     document.getElementById("Keywords_and_Constructors").innerHTML =
     "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
+
+function Coffee(Drink, Size, Milk, Temp) {
+    this.Coffee_Drink = Drink;
+    this.Coffee_Size = Size;
+    this.Coffee_Milk = Milk;
+    this.Coffee_Temp = Temp;
+}
+
+var customer = new Coffee("Mocha", 12 + "oz", "Whole", "Iced");
+
+function newFunction () {
+    document.getElementById("New_and_This").innerHTML =
+    "Customer order: " + customer.Coffee_Drink + customer.Coffee_Size + customer.Coffee_Milk + customer.Coffee_Temp;
 }
