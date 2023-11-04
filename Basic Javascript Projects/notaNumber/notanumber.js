4442c0c6d24b3d427575135dd528c040ffd996db
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 09:06:10 -0600

    assign submission proj5

--- Basic Javascript Projects/notaNumber/notanumber.js
+++ Basic Javascript Projects/notaNumber/notanumber.js
@@ -1,4 +1,4 @@
-console.log(2 + 2);
+console.log(2 + 2); //will display infinity
 function my_Function() {
   document.getElementById("Test").innerHTML = 0 / 0;
 }
@@ -52,8 +52,8 @@ document.write(5 > 10 || 10 > 4);
 document.write(5 >  10 || 10 > 20);
 
 function not_Function() {
-  document.getElementById("Not").innerHTML = ! (20>10);
+  document.getElementById("Not").innerHTML = ! (20>10); //making a true statement
 }
 function not_Function() {
-  document.getElementById("Not").innerHTML = ! (5>10);
+  document.getElementById("Not").innerHTML = ! (5>10); //making a false statement
 }
