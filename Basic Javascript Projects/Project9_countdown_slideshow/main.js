8b8cd537b97a8891e14928ddc655354f4d49096b
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-11-04 17:06:50 -0600

    saving

--- Basic Javascript Projects/Project9_countdown_slideshow/main.js
+++ Basic Javascript Projects/Project9_countdown_slideshow/main.js
@@ -1,16 +1,8 @@
 function countdown() {
-    var seconds = document.getElementById("minutes").value;
+    // var seconds = document.getElementById("minutes").value;
     var seconds = document.getElementById("seconds").value;
 
-
-    function tick() {
-        minutes = minutes -1;
-        timer.innerHTML = minutes;
-        var time = setTimeout(tick, 1000);
-        if (minutes == -1){
-        tick();
-    }
-        function tick() {
+  function tick() {
             seconds = seconds -1;
             timer.innerHTML = seconds;
             var time = setTimeout(tick, 1000);
