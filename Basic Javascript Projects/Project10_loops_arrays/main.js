6ee86072ad905346ecd3f063e40df1123608522f
AnnieGlade4327 <andreaglade4327@gmail.com>
2024-01-01 12:46:41 -0700

    added successful array funciton

--- Basic Javascript Projects/Project10_loops_arrays/main.js
+++ Basic Javascript Projects/Project10_loops_arrays/main.js
@@ -11,3 +11,13 @@ let text = "Hello World!";
 let length = text.length;
 
 document.getElementById("demo").innerHTML = length;
+
+function animal_Pics() {
+    var Animal_Picture = [];
+    Animal_Picture[0] = "Australian Cattle Dog";
+    Animal_Picture[1] = "Clarks Nutcracker";
+    Animal_Picture[2] = "Pronghorn Antelope";
+    Animal_Picture[3] = "Mountain Lion";
+    document.getElementById("Animal").innerHTML = "This is a picture of a " + Animal_Picture[2] + ".";
+    
+}
